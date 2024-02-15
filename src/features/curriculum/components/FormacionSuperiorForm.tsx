1c24d51eb704b9bdad7f6a446c00a903491de041
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-02-15 11:48:09 -0500

    update move useFieldArray to its own component

--- src/features/curriculum/components/FormacionSuperiorForm.tsx
+++ src/features/curriculum/components/FormacionSuperiorForm.tsx
@@ -1,6 +1,8 @@
 import * as yup from "yup";
 import "./FormComponents.css";
 import { ReactNode } from "react";
+import { useFieldArray } from "react-hook-form";
+import Box from "../../../shared/containers/Box/Box";
 import Input from "../../../shared/components/form/Input";
 import Select from "../../../shared/components/form/Select";
 import { FormacionSuperior } from "../../../models/CurriculumNuevoInterface";
@@ -25,7 +27,6 @@ interface InputProps {
     control: any;
     setValue: any;
     register: any;
-    index: any;
 }
 
 export const formacionSuperiorDefaultValues: FormacionSuperior = {
@@ -60,59 +61,88 @@ export const formacionSuperiorSchema = yup.object().shape({
 
 export default function FormacionSuperiorForm(props: InputProps): ReactNode {
 
-    const { errors, control, setValue, register, index } = props;
+    const { errors, control, setValue, register } = props;
+
+    const { fields, append, remove } = useFieldArray({
+        name: "formacionSuperior",
+        control
+    });
+
+    function handleAppend(event: any) {
+        event.preventDefault();
+        append({
+            modalidadAcademica: "",
+            numeroSemestresAprobados: "",
+            graduado: "",
+            tituloObtenido: "",
+            fechaTerminacion: new Date(),
+            numeroTarjetaProfesional: "",
+        });
+    }
 
     return (
-        <div className="row">
-            <Select
-                register={register}
-                label="Modalidad académica"
-                wrapperClass="mb-3 col-md-4"
-                options={modalidadAcademicaOptions}
-                name={`formacionSuperior.${index}.modalidadAcademica`}
-                error={errors.formacionSuperior && errors.formacionSuperior[index]?.modalidadAcademica?.message}
-            />
-            <Input
-                type="text"
-                register={register}
-                wrapperClass="mb-3 col-md-4"
-                label="Número semestres aprobados"
-                name={`formacionSuperior.${index}.numeroSemestresAprobados`}
-                error={errors.formacionSuperior && errors.formacionSuperior[index]?.numeroSemestresAprobados?.message}
-            />
-            <Select
-                register={register}
-                label="Graduado"
-                options={SiNoOptions}
-                wrapperClass="mb-3 col-md-4"
-                name={`formacionSuperior.${index}.graduado`}
-                error={errors.formacionSuperior && errors.formacionSuperior[index]?.graduado?.message}               
-            />
-            <Input
-                type="text"
-                register={register}
-                label="Título obtenido"
-                wrapperClass="mb-3 col-md-4"
-                name={`formacionSuperior.${index}.tituloObtenido`}
-                error={errors.formacionSuperior && errors.formacionSuperior[index]?.tituloObtenido?.message}
-            />
-            <CustomDatePicker
-                control={control}
-                register={register}
-                setValue={setValue}
-                wrapperClass="mb-3 col-md-4"
-                label="Fecha de terminación"
-                name={`formacionSuperior.${index}.fechaTerminacion`}
-                error={errors.formacionSuperior && errors.formacionSuperior[index]?.fechaTerminacion?.message}                
-            />
-            <Input
-                type="text"
-                register={register}
-                wrapperClass="mb-3 col-md-4"
-                label="Número tarjeta profesional"
-                name={`formacionSuperior.${index}.numeroTarjetaProfesional`}
-                error={errors.formacionSuperior && errors.formacionSuperior[index]?.numeroTarjetaProfesional?.message}                                
-            />
-        </div>
+        <Box wrapperClass="row">
+            <header className="inline-flex">
+                <h4 className="mb-4">Formación Superior</h4>
+                <button onClick={handleAppend} className="btn btn-sm btn-success">Add</button>
+            </header>
+
+            {fields.map((field, index) => (
+                <div key={field.id} className={index > 0 ? "dynamic-fields" : ""}>
+                    <div className="row">
+                        <Select
+                            register={register}
+                            label="Modalidad académica"
+                            wrapperClass="mb-3 col-md-4"
+                            options={modalidadAcademicaOptions}
+                            name={`formacionSuperior.${index}.modalidadAcademica`}
+                            error={errors.formacionSuperior && errors.formacionSuperior[index]?.modalidadAcademica?.message}
+                        />
+                        <Input
+                            type="text"
+                            register={register}
+                            wrapperClass="mb-3 col-md-4"
+                            label="Número semestres aprobados"
+                            name={`formacionSuperior.${index}.numeroSemestresAprobados`}
+                            error={errors.formacionSuperior && errors.formacionSuperior[index]?.numeroSemestresAprobados?.message}
+                        />
+                        <Select
+                            register={register}
+                            label="Graduado"
+                            options={SiNoOptions}
+                            wrapperClass="mb-3 col-md-4"
+                            name={`formacionSuperior.${index}.graduado`}
+                            error={errors.formacionSuperior && errors.formacionSuperior[index]?.graduado?.message}
+                        />
+                        <Input
+                            type="text"
+                            register={register}
+                            label="Título obtenido"
+                            wrapperClass="mb-3 col-md-4"
+                            name={`formacionSuperior.${index}.tituloObtenido`}
+                            error={errors.formacionSuperior && errors.formacionSuperior[index]?.tituloObtenido?.message}
+                        />
+                        <CustomDatePicker
+                            control={control}
+                            register={register}
+                            setValue={setValue}
+                            wrapperClass="mb-3 col-md-4"
+                            label="Fecha de terminación"
+                            name={`formacionSuperior.${index}.fechaTerminacion`}
+                            error={errors.formacionSuperior && errors.formacionSuperior[index]?.fechaTerminacion?.message}
+                        />
+                        <Input
+                            type="text"
+                            register={register}
+                            wrapperClass="mb-3 col-md-4"
+                            label="Número tarjeta profesional"
+                            name={`formacionSuperior.${index}.numeroTarjetaProfesional`}
+                            error={errors.formacionSuperior && errors.formacionSuperior[index]?.numeroTarjetaProfesional?.message}
+                        />
+                    </div>
+                    {index > 0 && <i onClick={() => remove(index)} className="icon bx bx-trash-alt" title="Eliminar"></i>}
+                </div>
+            ))}
+        </Box>
     )
 }
--- src/features/curriculum/pages/curriculum-nuevo/CurriculumNuevo.tsx
+++ src/features/curriculum/pages/curriculum-nuevo/CurriculumNuevo.tsx
@@ -1,7 +1,6 @@
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import Box from "../../../../shared/containers/Box/Box";
-import { FieldErrors, useForm, useFieldArray } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 import { CurriculumNuevoInterface } from "../../../../models/CurriculumNuevoInterface";
 import DatosPersonalesForm, { datosPersonalesDefaultValues, datosPersonalesSchema } from "../../components/DatosPersonalesForm";
 import FormacionBasicaForm, { formacionBasicaDefaultValues, formacionBasicaSchema } from "../../components/FormacionBasicaForm";
@@ -30,25 +29,8 @@ export default function CurriculumNuevo() {
     const { register, formState, handleSubmit, control, setValue } = form;
     const { errors } = formState;
 
-    const { fields, append, remove } = useFieldArray({
-        name: "formacionSuperior",
-        control
-    });
-
     function onError(errors: FieldErrors<CurriculumNuevoInterface>) {
         console.log(errors);
-    }
-    
-    function handleAppend(event: any){
-        event.preventDefault();
-        append({ 
-            modalidadAcademica: "",
-            numeroSemestresAprobados: "",
-            graduado: "",
-            tituloObtenido: "",
-            fechaTerminacion: new Date(),
-            numeroTarjetaProfesional: "",
-        });
     }
 
     function onSubmit(data: any) {
@@ -77,27 +59,12 @@ export default function CurriculumNuevo() {
                     register={register}
                     setValue={setValue}
                 />
-
-                <Box wrapperClass="row">
-                    <header className="inline-flex">
-                        <h4 className="mb-4">Formación Superior</h4>
-                        <button onClick={handleAppend} className="btn btn-sm btn-success">Add</button>
-                    </header>
-
-                    {fields.map((field, index) => (
-                        <div key={field.id} className={index > 0 ? "dynamic-fields" : ""}>
-                            <FormacionSuperiorForm
-                                index={index}
-                                errors={errors}
-                                control={control}
-                                register={register}
-                                setValue={setValue}
-                            />
-                            {index > 0 && <i onClick={() => remove(index)} className="icon bx bx-trash-alt" title="Eliminar"></i>}
-                        </div>
-                    ))}
-                </Box>
-
+                <FormacionSuperiorForm
+                    errors={errors}
+                    control={control}
+                    register={register}
+                    setValue={setValue}
+                />
                 <button type="submit" className="btn btn-primary">Crear</button>
             </form>
         </>
