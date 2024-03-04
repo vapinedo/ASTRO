a2d8c91707c24b51aec3bf558293cebd87eec60e
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-03-04 09:11:14 -0500

    Fix .env file issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
-import React from 'react';
-import AppRouting from './AppRouting'
-import { CssBaseline } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
-import "./firebaseConfig"; // needed to proper initialice all firebase services
+import "./App.css"
+import React from "react";
+import AppRouting from "./AppRouting"
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import "../firebaseConfig"; // needed to proper initialice all firebase services
 
 export default function App() {
     return (
