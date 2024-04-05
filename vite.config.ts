c2f4b25efa9ff58c071c45a0c381471fbcbe93ab
vpinedo-neostella <victor.pinedo@neostella.com>
2024-04-05 14:49:28 -0500

    feat(global): add absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "./",
+  resolve: {
+    alias: {
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@helpers": path.resolve(__dirname, "./src/helpers"),
+      "@redux": path.resolve(__dirname, "./src/redux"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+});
